b66bb531a05e0eefbb0cebd6d34a0ba2d408302c
smit patel <bhalodiyasmit2409@gmail.com>
2025-03-21 12:21:02 +0530

    Update GitHub workflows for linting and testing; streamline dependency installation and update Node.js version

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -5,22 +5,23 @@ use Livewire\Volt\Volt;
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('registration screen can be rendered', function () {
-    $response = $this->get('/register');
+  $response = $this->get('/register');
 
-    $response->assertStatus(200);
+  $response->assertStatus(200);
 });
 
 test('new users can register', function () {
-    $response = Volt::test('auth.register')
-        ->set('name', 'Test User')
-        ->set('email', 'test@example.com')
-        ->set('password', 'password')
-        ->set('password_confirmation', 'password')
-        ->call('register');
+  $response = Volt::test('auth.register')
+    ->set('name', 'Test User')
+    ->set('email', 'test@example.com')
+    ->set('password', 'password')
+    ->set('password_confirmation', 'password')
+    ->set('terms', true)  // Add this line to set the terms checkbox to true
+    ->call('register');
 
-    $response
-        ->assertHasNoErrors()
-        ->assertRedirect(route('dashboard', absolute: false));
+  $response
+    ->assertHasNoErrors()
+    ->assertRedirect(route('dashboard', absolute: false));
 
-    $this->assertAuthenticated();
+  $this->assertAuthenticated();
 });
